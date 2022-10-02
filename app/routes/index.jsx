cbc9678ca7ad09ec7dd7692c27aa00941861455e
Kasra Rahjerdi <hi@kasra.codes>
2022-10-02 09:52:25 -0700

    Re-try staggered design look

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,20 +1,25 @@
+import styles from "../styles/index.css";
 
-const RecommendationTile = ({position, title, author, publicationYear, children}) => (
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
+
+const RecommendationTile = ({position, title, author, publicationYear, children, cover}) => (
   <div className='mb-24 mx-4 md:mx-0 relative'>
     <div className='flex flex-col md:flex-row items-left md:items-center'>
       <div className='w-4/5'>
-        <img src='https://i.imgur.com/ohjK97K.jpg' className='w-100 drop-shadow-2xl' />
+        <img src={cover} className='w-100 drop-shadow-2xl blur-xxs' />
       </div>
-      <div className='absolute w-4/5 pr-0 md:pr-14 top-1/3 md:top-56 left-16 md:left-1/4'>
+      <div className='absolute top-1/3 md:top-56 book-title'>
         <p className='text-3xl md:text-5xl font-gothic font-bold mt-2 md:mt-0 text-black break-words'>
         {title}
         </p>
-        {/*
+
         <p className='text-base font-sans font-normal mt-1'>
           {author}, {publicationYear}
         </p>
-        */}
-        <p className='mt-4 text-base max-w-prose text-justify hidden'>
+
+        <p className='mt-4 text-sm ml-8 text-right hidden'>
           {children}
         </p>
       </div>
@@ -25,12 +30,12 @@ const RecommendationTile = ({position, title, author, publicationYear, children}
 export default function Index() {
   return (
     <div className='bg-ocean v-full h-full flex py-12 items-center flex-col'>
-      <p className='text-4xl flex'>
+      <p className='text-4xl flex text-center font-gothic'>
         <span className='text-2xl mr-4'>🚩</span> Kasra's Red Flag List <span className='text-2xl ml-4'>🚩</span>
       </p>
       
-      <p className='text-l max-w-sm md:max-w-prose text-center md:text-left my-12'>
-        Hi all! the following is a list of my favorite books, which I also consider extremely big red flags if someone else says they're their favorite books.
+      <p className='text-l max-w-sm md:max-w-prose text-center my-12'>
+        Hi. Here is a list of my favorite books, which I also consider extremely big red flags if someone else says they're their favorite books.
       </p>
 
       <div className='max-w-2xl'>
@@ -39,6 +44,7 @@ export default function Index() {
           title={'The Rebel: An Essay on Man in Revolt'}
           author={'Albert Camus'}
           publicationYear={1951}
+          cover={'https://i.imgur.com/ohjK97K.jpg'}
         >
           This is less a book and more of a textbook, it contains the main thesis that sets the tone for Camus' novels. It goes over every example of rebellion starting with Prometheus and examines their structure and purpose. 
         </RecommendationTile>
@@ -48,6 +54,7 @@ export default function Index() {
           title={'The Nick Adams Stories'}
           author={'Ernest Hemingway'}
           publicationYear={1966}
+          cover={'https://i.imgur.com/Gb1NSat.jpg'}
         >
           A series of Hemingway short stories, some throw-away and some nuggets that were later refined in his other books. What I appreciate about this set is that the stories mostly focus on younger people compared than his novels.
         </RecommendationTile>
@@ -57,6 +64,7 @@ export default function Index() {
           title={'Demian'}
           author={'Herman Hesse'}
           publicationYear={1919}
+          cover={'https://i.imgur.com/xnHgOCi.png'}
         >
           In the same vein as why I appreciate the previous book, Demian is the ideal bildungsroman. Originally published anonymously, it's the outcome of Hesse spending a lot of time with Carl Jung. A very fun book that I come back to every time I feel like I'm "coming of age" (which is a lot.)
         </RecommendationTile>
@@ -66,6 +74,7 @@ export default function Index() {
           title={'Exile and the Kingdom'}
           author={'Albert Camus'}
           publicationYear={1957}
+          cover={'https://i.imgur.com/zXRFDKs.png'}
         >
           PSYCH! We're back on Camus! You thought it was over!
           <br /><br />
